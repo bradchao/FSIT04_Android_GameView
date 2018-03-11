5e47525e3b714227290b6a83bfb3f2c85c457585
Brad <brad@brad.tw>
2018-03-11 15:42:40 +0800

    v1

---
+++ app/src/main/java/tw/org/iii/thread2/GameView.java
@@ -0,0 +1,22 @@
+package tw.org.iii.thread2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2018/3/11.
+ */
+
+public class GameView extends View {
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
