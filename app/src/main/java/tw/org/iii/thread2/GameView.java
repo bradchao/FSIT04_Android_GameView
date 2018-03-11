e039e24617780d2937df96300231a0a4ff0f393a
Brad <brad@brad.tw>
2018-03-11 16:58:57 +0800

    v5

--- app/src/main/java/tw/org/iii/thread2/GameView.java
+++ app/src/main/java/tw/org/iii/thread2/GameView.java
@@ -11,6 +11,9 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by Administrator on 2018/3/11.
  */
@@ -18,18 +21,22 @@ import android.view.View;
 public class GameView extends View {
     private Context context;
     private Resources res;
-    private float viewW, viewH, ballW, ballH;
+    private float viewW, viewH, ballW, ballH, ballX, ballY, dx, dy;
     private boolean isInit;
     private Bitmap ballBmp;
+    private Timer timer;
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
         res = context.getResources();
+        timer = new Timer();
+        dx = dy = 16;
         setBackgroundResource(R.drawable.bg2);
     }
 
     private void init(){
+        isInit = true;
         viewW = getWidth();
         viewH = getHeight();
         ballW = viewW / 12; ballH = ballW;
@@ -41,18 +48,33 @@ public class GameView extends View {
 
         matrix.reset();
         matrix.postScale(ballW / ow, ballH / oh);
-        ballBmp = Bitmap.createBitmap(ballBmp,0,0,(int)ow,(int)oh,matrix,false);
+        ballBmp = Bitmap.createBitmap(ballBmp,0,0,
+                (int)ow,(int)oh,matrix,false);
 
-
+        timer.schedule(new BallTask(), 1000, 80);
     }
 
+    private class BallTask extends TimerTask {
+        @Override
+        public void run() {
+            if (ballX < 0 || ballX + ballW > viewW){
+                dx *= -1;
+            }
+            if (ballY <0 || ballY + ballH > viewH){
+                dy *= -1;
+            }
+            ballX += dx; ballY += dy;
+            //invalidate();
+            postInvalidate();
+        }
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if (!isInit) init();
 
-        canvas.drawBitmap(ballBmp,0,0,null);
+        canvas.drawBitmap(ballBmp,ballX,ballY,null);
 
     }
 }
